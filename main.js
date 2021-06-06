ab1dd0ec374be448b18e7419d0f5d8bb15f3697a
Arjun Yuvaraj <arjunycoding@gmail.com>
2021-06-06 11:56:01 -0400

    generating random plastic

--- main.js
+++ main.js
@@ -10,4 +10,14 @@ let images = [
     "img/plastic3.png",
     "img/plastic4.png",
     "img/plastic5.png",
-    "img/plastic6.png"]
+    "img/plastic6.png"
+]
+let randomIndex = Math.floor(Math.random() * 6);
+let randomIndex2 = Math.floor(Math.random() * 6);
+let randomIndex3 = Math.floor(Math.random() * 6);
+let trash1 = $("#trash1")
+let trash2 = $("#trash2")
+let trash3 = $("#trash3")
+trash1.attr( "src", images[randomIndex])
+trash2.attr( "src", images[randomIndex2])
+trash3.attr( "src", images[randomIndex3])
