79e823922d7380e7ebf3de98f911d7c6fc65785a
Arjun Yuvaraj <arjunycoding@gmail.com>
2021-06-06 11:50:00 -0400

    addding 3 nre pices of plastic and and an array with those images

--- main.js
+++ main.js
@@ -4,36 +4,10 @@ let keyNames = {
     37: "left",
     39: "right"
 }
-let fish = document.querySelector("#fish");
-let fishStyles = fish.style;
-function moveUp(){
-    fishStyles.top += 2 + "px";
-
-}
-function moveDown(){
-    fishStyles.top -= 2 + "px";
-
-}
-function moveLeft(){
-    fishStyles.left += 2 + "px";
-
-}
-function moveRight(){
-    fishStyles.letf -= 2 + "px";
-
-}
-function moveFish(event){
-    let key = keyNames[event.keyCode];
-
-    if(key == "up"){
-        moveUp();
-    } else if(key == "down"){
-        moveDown()
-    } else if(key == "right"){
-        moveRight()
-    } else if (key == "left"){
-        moveLeft()
-    }
-}
-
-$("body").keydown(moveFish);
+let images = [
+    "img/plastic1.png", 
+    "img/plastic2.png", 
+    "img/plastic3.png",
+    "img/plastic4.png",
+    "img/plastic5.png",
+    "img/plastic6.png"]
