0458b5787156a2e525ae8735c665da920b979eca
Arjun Yuvaraj <arjunycoding@gmail.com>
2021-06-19 11:22:55 -0400

    making the fish move up and down but will NOT leave the fish tank!

--- main.js
+++ main.js
@@ -6,20 +6,13 @@ let images = [
     "img/plastic5.png",
     "img/plastic6.png"
 ]
-var keyNames = {
-    32: "space",
-    37: "left",
-    38: "up",
-    39: "right",
-    40: "down"
-};
 let randomIndex = Math.floor(Math.random() * 6);
 let randomIndex2 = Math.floor(Math.random() * 6);
 let randomIndex3 = Math.floor(Math.random() * 6);
 let trash1 = $("#trash1")
 let trash2 = $("#trash2")
 let trash3 = $("#trash3")
-let fish = document.querySelector("#fish")
+let fish = $("#fish")
 for(let i = 0; i <= 5; i++){
     if(randomIndex == randomIndex2 || randomIndex == randomIndex3){
         randomIndex = Math.floor(Math.random() * 6);
@@ -34,8 +27,30 @@ for(let i = 0; i <= 5; i++){
 trash1.attr( "src", images[randomIndex])
 trash2.attr( "src", images[randomIndex2])
 trash3.attr( "src", images[randomIndex3])
+let y = 150 ;
+let yPos = y + "px";
+function eventHandler(event){
+    if(event.keyCode == 38){
+        y -= 10;
+        yPos = y + "px";
+        fish.css("top", yPos)
+        console.log(yPos)
+        if(yPos == 70 + "px"){
+            y += 10
+        }
+    } else if(event.keyCode == 40){
+        y += 10;
+        yPos = y + "px";
+        fish.css("top", yPos)
+        console.log(yPos)
 
-$("body").keydown(function(event) {
-    if(event)
-        fish.classList.add("fishLeft")
-})
+        if(yPos == 330 + "px"){
+            y -= 10
+        }
+    }
+}
+$("body").keydown(eventHandler)
+
+// keyNames
+//     38: "up",
+//     40: "down"
